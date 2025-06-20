eafd33f20384598c3ccc910ecb199bf9c99061f7
Cristian <crisarag8.sal@gmail.com>
2025-06-20 12:51:47 -0600

    Add initial command implementation for the bot with a ping command

---
+++ bot/commands/general.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+
+class GeneralCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        await ctx.send("🏓 Pong!")
