40d7c9a21d52e2cb9c7d248dea3927380fe67358
Cristian <crisarag8.sal@gmail.com>
2025-06-20 14:25:09 -0600

    Refactor bot command loading to use extensions for better modularity

--- bot/main.py
+++ bot/main.py
@@ -2,24 +2,23 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-
 from bot import config
 
 def run_bot():
     load_dotenv()
     intents = discord.Intents.default()
     intents.message_content = True
+
     bot = commands.Bot(command_prefix=config.BOT_PREFIX, intents=intents)
-
-    # Carga de comandos (cogs)
-    from bot.commands.music import MusicCommands
-    from bot.commands.general import GeneralCommands
-
-    bot.add_cog(MusicCommands(bot))
-    bot.add_cog(GeneralCommands(bot))
 
     @bot.event
     async def on_ready():
         print(f"✅ Conectado como {bot.user}")
 
+    async def load():
+        await bot.load_extension("bot.commands.general")
+        await bot.load_extension("bot.commands.music")
+
+    import asyncio
+    asyncio.run(load())
     bot.run(config.DISCORD_TOKEN)
